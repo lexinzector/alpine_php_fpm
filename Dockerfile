5e25c336cfe18569dfe835bff164ff76929679a3
lexinzector <lexin.ucoz.ru@gmail.com>
2025-03-18 18:28:36 +0500

    edit Dockerfile and build.sh

--- Dockerfile
+++ Dockerfile
@@ -1,17 +1,16 @@
-ARG ARCH=
+ARG ARCH=""
 FROM ${ARCH}alpine:3.8
 
 RUN cd ~; \
-	apk update; \
-	apk upgrade; \
-	apk add bash nano mc wget net-tools pv zip unzip supervisor procps grep sudo; \
+	apk update --no-cache; \
+	apk upgrade --no-cache; \
+	apk add --no-cache bash nano mc wget net-tools pv zip unzip supervisor procps grep sudo; \
 	rm -rf /var/cache/apk/*; \
 	echo "export EDITOR=nano" > /etc/profile.d/editor_nano; \
 	echo "Ok"
 
 RUN cd ~; \
-	apk update; \
-	apk add php5 php5-fpm php5-json php5-openssl php5-pdo_mysql php5-curl php5-phar php5-bcmath php5-sockets php5-mysqli php5-soap php5-ctype php5-iconv php5-dom php5-gd php5-mysql php5-exif php5-zip php5-xml php5-xmlreader php5-opcache php5-apcu php5-mcrypt php5-intl php5-pdo_mysql php5-pdo_sqlite php5-sqlite3 php5-pgsql php5-cgi php5-embed php5-xmlrpc php5-dbg php5-sysvshm php5-imap php5-doc php5-common php5-calendar php5-pdo_dblib php5-dba php5-odbc php5-shmop php5-wddx php5-cli php5-suhosin php5-phpdbg php5-bz2 php5-sysvmsg php5-pspell php5-dev php5-ftp php5-gettext php5-mssql php5-pcntl php5-pear php5-pdo_pgsql php5-pdo php5-sysvsem php5-posix php5-xsl php5-ldap php5-pdo_odbc php5-enchant php5-gmp php5-snmp curl nginx mysql-client; \
+	apk add --no-cache php5 php5-fpm php5-json php5-openssl php5-pdo_mysql php5-curl php5-phar php5-bcmath php5-sockets php5-mysqli php5-soap php5-ctype php5-iconv php5-dom php5-gd php5-mysql php5-exif php5-zip php5-xml php5-xmlreader php5-opcache php5-apcu php5-mcrypt php5-intl php5-pdo_mysql php5-pdo_sqlite php5-sqlite3 php5-pgsql php5-cgi php5-embed php5-xmlrpc php5-dbg php5-sysvshm php5-imap php5-doc php5-common php5-calendar php5-pdo_dblib php5-dba php5-odbc php5-shmop php5-wddx php5-cli php5-suhosin php5-phpdbg php5-bz2 php5-sysvmsg php5-pspell php5-dev php5-ftp php5-gettext php5-mssql php5-pcntl php5-pear php5-pdo_pgsql php5-pdo php5-sysvsem php5-posix php5-xsl php5-ldap php5-pdo_odbc php5-enchant php5-gmp php5-snmp curl nginx mysql-client; \
 	rm -rf /var/cache/apk/*; \
 	addgroup -g 1000 -S www; \
 	adduser -D -H -S -G www -u 1000 www; \
--- build.sh
+++ build.sh
@@ -1,80 +1,81 @@
 #!/bin/bash
 
-SCRIPT=$(readlink -f $0)
-SCRIPT_PATH=`dirname $SCRIPT`
-BASE_PATH=`dirname $SCRIPT_PATH`
+SCRIPT=$(readlink -f "$0")
+SCRIPT_PATH=$(dirname "$SCRIPT")
+BASE_PATH=$(dirname "$SCRIPT_PATH")
 
 RETVAL=0
 VERSION=5.6
 SUBVERSION=3
 IMAGE_NAME="alpine_php_fpm"
-TAG=`date '+%Y%m%d_%H%M%S'`
+TAG=$(date '+%Y%m%d_%H%M%S')
+REGISTRY="docker.io/lexinzector"
+
+build_and_push() {
+    local PLATFORM=$1
+    local ARCH=$2
+    local ARCH_ARG=""
+
+    # ARCH для Alpine (amd64 не требует префикса)
+    if [[ "$ARCH" == "arm64v8" || "$ARCH" == "arm32v6" ]]; then
+        ARCH_ARG="${ARCH}/"
+    fi
+
+    local FULL_TAG="$REGISTRY/$IMAGE_NAME:$VERSION-$SUBVERSION-$ARCH"
+
+    echo "Building for $PLATFORM ($ARCH)..."
+    docker buildx build --platform "$PLATFORM" -t "$FULL_TAG" --file Dockerfile . --build-arg ARCH="$ARCH_ARG" --push
+}
+
+create_manifest() {
+    echo "Creating and pushing multi-arch manifest..."
+    docker buildx imagetools create -t "$REGISTRY/$IMAGE_NAME:$VERSION-$SUBVERSION" \
+        "$REGISTRY/$IMAGE_NAME:$VERSION-$SUBVERSION-amd64" \
+        "$REGISTRY/$IMAGE_NAME:$VERSION-$SUBVERSION-arm64v8" \
+        "$REGISTRY/$IMAGE_NAME:$VERSION-$SUBVERSION-arm32v6"
+
+    docker buildx imagetools create -t "$REGISTRY/$IMAGE_NAME:$VERSION" \
+        "$REGISTRY/$IMAGE_NAME:$VERSION-$SUBVERSION-amd64" \
+        "$REGISTRY/$IMAGE_NAME:$VERSION-$SUBVERSION-arm64v8" \
+        "$REGISTRY/$IMAGE_NAME:$VERSION-$SUBVERSION-arm32v6"
+}
 
 case "$1" in
-	
-	test)
-		docker build ./ -t lexinzector/$IMAGE_NAME:$VERSION-$SUBVERSION-$TAG \
-			--file Dockerfile
-	;;
-	
-	amd64)
-		export DOCKER_DEFAULT_PLATFORM=linux/amd64
-		docker build ./ -t lexinzector/$IMAGE_NAME:$VERSION-$SUBVERSION-amd64 \
-			--file Dockerfile --build-arg ARCH=amd64/
-	;;
-	
-	arm64v8)
-		export DOCKER_DEFAULT_PLATFORM=linux/arm64/v8
-		docker build ./ -t lexinzector/$IMAGE_NAME:$VERSION-$SUBVERSION-arm64v8 \
-			--file Dockerfile --build-arg ARCH=arm64v8/
-	;;
-	
-	arm32v6)
-		export DOCKER_DEFAULT_PLATFORM=linux/arm/v6
-		docker build ./ -t lexinzector/$IMAGE_NAME:$VERSION-$SUBVERSION-arm32v6 \
-			--file Dockerfile --build-arg ARCH=arm32v6/
-	;;
-	
-	manifest)
-		rm -rf ~/.docker/manifests/docker.io_lexinzector_$IMAGE_NAME-*
-		
-		docker tag lexinzector/$IMAGE_NAME:$VERSION-$SUBVERSION-amd64 lexinzector/$IMAGE_NAME:$VERSION-amd64
-		docker tag lexinzector/$IMAGE_NAME:$VERSION-$SUBVERSION-arm64v8 lexinzector/$IMAGE_NAME:$VERSION-arm64v8
-		docker tag lexinzector/$IMAGE_NAME:$VERSION-$SUBVERSION-arm32v6 lexinzector/$IMAGE_NAME:$VERSION-arm32v6
-		
-		docker push lexinzector/$IMAGE_NAME:$VERSION-$SUBVERSION-amd64
-		docker push lexinzector/$IMAGE_NAME:$VERSION-$SUBVERSION-arm64v8
-		docker push lexinzector/$IMAGE_NAME:$VERSION-$SUBVERSION-arm32v6
-		
-		docker push lexinzector/$IMAGE_NAME:$VERSION-amd64
-		docker push lexinzector/$IMAGE_NAME:$VERSION-arm64v8
-		docker push lexinzector/$IMAGE_NAME:$VERSION-arm32v6
-		
-		docker manifest create lexinzector/$IMAGE_NAME:$VERSION-$SUBVERSION \
-			--amend lexinzector/$IMAGE_NAME:$VERSION-$SUBVERSION-amd64 \
-			--amend lexinzector/$IMAGE_NAME:$VERSION-$SUBVERSION-arm64v8 \
-			--amend lexinzector/$IMAGE_NAME:$VERSION-$SUBVERSION-arm32v6
-		docker manifest push lexinzector/$IMAGE_NAME:$VERSION-$SUBVERSION
-		
-		docker manifest create lexinzector/$IMAGE_NAME:$VERSION \
-			--amend lexinzector/$IMAGE_NAME:$VERSION-amd64 \
-			--amend lexinzector/$IMAGE_NAME:$VERSION-arm64v8 \
-			--amend lexinzector/$IMAGE_NAME:$VERSION-arm32v6
-		docker manifest push lexinzector/$IMAGE_NAME:$VERSION
-	;;
-	
-	all)
-		$0 amd64
-		$0 arm64v8
-		$0 arm32v6
-		$0 manifest
-	;;
-	
-	*)
-		echo "Usage: $0 {amd64|arm64v8|arm32v6|manifest|all|test}"
-		RETVAL=1
+
+    test)
+        docker buildx build --platform linux/amd64 \
+            -t "$REGISTRY/$IMAGE_NAME:$VERSION-$SUBVERSION-$TAG" \
+            --file Dockerfile .
+    ;;
+
+    amd64)
+        build_and_push "linux/amd64" "amd64"
+    ;;
+
+    arm64v8)
+        build_and_push "linux/arm64/v8" "arm64v8"
+    ;;
+
+    arm32v6)
+        build_and_push "linux/arm/v6" "arm32v6"
+    ;;
+
+    manifest)
+        create_manifest
+    ;;
+
+    all)
+        $0 amd64
+        $0 arm64v8
+        $0 arm32v6
+        $0 manifest
+    ;;
+
+    *)
+        echo "Usage: $0 {amd64|arm64v8|arm32v6|manifest|all|test}"
+        RETVAL=1
+    ;;
 
 esac
 
 exit $RETVAL
-
